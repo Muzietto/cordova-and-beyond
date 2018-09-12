054d83e2b73f92c8148341b7e5f99124fdf17611
Marco Faustinelli (Muzietto) <marco_faustinelli@yahoo.com>
2018-09-12 09:36:38 +0200

    adding geolocation

--- www/js/EmployeeView.js
+++ www/js/EmployeeView.js
@@ -2,12 +2,27 @@ var EmployeeView = function(employee) {
 
   this.initialize = function() {
     this.$el = $('<div/>');
+    this.$el.on('click', '.add-location-btn', this.addLocation);
   }
 
   this.render = function() {
     this.$el.html(this.template(employee));
     return this;
   }
+
+  this.addLocation = function(event) {
+    event.preventDefault();
+    navigator.geolocation.getCurrentPosition(
+        function(position) {
+            alert(position.coords.latitude + ',' + position.coords.longitude);
+        },
+        function(err) {
+            alert('Error getting location: ' + JSON.stringify(err));
+        },
+        { timeout: 10 }
+    );
+    return false;
+  };
 
   this.initialize();
 }
