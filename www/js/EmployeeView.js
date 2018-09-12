d7b7a8fe2e25fc39d530d3dbce6ede0f393872cd
Marco Faustinelli (Muzietto) <marco_faustinelli@yahoo.com>
2018-09-12 09:55:39 +0200

    adding contacts plugin

--- www/js/EmployeeView.js
+++ www/js/EmployeeView.js
@@ -3,6 +3,7 @@ var EmployeeView = function(employee) {
   this.initialize = function() {
     this.$el = $('<div/>');
     this.$el.on('click', '.add-location-btn', this.addLocation);
+    this.$el.on('click', '.add-contact-btn', this.addToContacts);
   }
 
   this.render = function() {
@@ -21,6 +22,33 @@ var EmployeeView = function(employee) {
         },
         { timeout: 10 }
     );
+    return false;
+  };
+
+  this.addToContacts = function(event) {
+    event.preventDefault();
+    console.log('addToContacts');
+
+    if (!navigator.contacts) {
+        alert("Contacts API not supported", "Error");
+        return;
+    }
+
+    var contact = navigator.contacts.create();
+
+    if (!contact) {
+        alert("Contacts API not supported", "Error");
+        return;
+    }
+
+    contact.name = {givenName: employee.firstName, familyName: employee.lastName};
+    var phoneNumbers = [];
+    phoneNumbers[0] = new ContactField('work', employee.officePhone, false);
+    phoneNumbers[1] = new ContactField('mobile', employee.cellPhone, true);
+    contact.phoneNumbers = phoneNumbers;
+
+    contact.save();
+
     return false;
   };
 
