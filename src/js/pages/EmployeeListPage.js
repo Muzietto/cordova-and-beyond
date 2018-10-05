650e60cbbeeddc7ba233c512aba11f0478f52032
Marco Faustinelli (Muzietto) <marco_faustinelli@yahoo.com>
2018-10-05 06:13:40 +0200

    using Link, not a

--- src/js/pages/EmployeeListPage.js
+++ src/js/pages/EmployeeListPage.js
@@ -1,14 +1,16 @@
 import React from 'react';
 
+import Link from 'app/js/services/routing/Link';
+
 const employeeMapper = ({id, pic, firstName, lastName, title}, index) => (
   <li className="table-view-cell media" key={`emp_${index}`}>
-    <a href={`#employees/${id}`}>
+    <Link href={`#employees/${id}`}>
       <img className="media-object pull-left" src={`assets/pics/${pic}`} />
       <div className="media-body">
         {firstName} {lastName}
         <p>{title}</p>
       </div>
-    </a>
+    </Link>
   </li>
 );
 
